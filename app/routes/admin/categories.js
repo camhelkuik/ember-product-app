12aa0b34b63e1460029001872a6ed353df93ff78
Cameron Helkuik <cameron@travefy.com>
2017-05-02 15:17:30 -0500

    have categories being filled in with fake info, can't delete

--- app/routes/admin/categories.js
+++ app/routes/admin/categories.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    model(){
+        return this.store.findAll('category');
+    },
+
+    actions: {
+        addNewCategory(id, name) {
+            this.store.createRecord('category', {id, name}).save();
+        },
+
+        deleteCategory(category) {
+            category.destoryRecord();
+        }
+    }
 });
